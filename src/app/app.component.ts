3daddb6f755ac5c6cf995350334c43f9aa833810
Manoj <manoj09633@gmail.com>
2022-10-05 14:26:44 +0530

    Error and loading screens introduced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable, of, Subject } from 'rxjs';
 import { AdviceService } from './services/advice.service';
 
 @Component({
@@ -10,6 +11,8 @@ export class AppComponent implements OnInit {
 
   id: number = 1;
   advice: string = "It is never too late to be who you might have been";
+  advice$?: Observable<any>;
+  loadingError$ = new Subject<boolean>();
 
   constructor(private adviceService: AdviceService) {
 
@@ -20,12 +23,17 @@ export class AppComponent implements OnInit {
   }
 
   getAdvice() {
-    this.adviceService.getAdvice().subscribe(response => {
-      this.id = response.slip.id;
-      this.advice = response.slip.advice;
-    });
+    this.advice$ = this.adviceService.getAdvice().pipe(
+      catchError((error) => {
+        console.log('error loading the advice', error);
+        this.loadingError$.next(true);
+        return of();
+      }));
   }
 
+  onReload() {
+    window.location.reload();
+  }
 
 
 }
